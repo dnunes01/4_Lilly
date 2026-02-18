74c088f7397b4be82421659958156cd8d8cd31d3
Dan Nunes <dnunes.utah@gmail.com>
2026-02-18 07:46:27 -0700

    added some style to nav bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { APP_NAME, TAB_TEXT } from './app.constants';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
