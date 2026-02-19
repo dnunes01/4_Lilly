2580e67f9c8ae026d48b1c9561ab4c959cfc86cc
Dan Nunes <dnunes.utah@gmail.com>
2026-02-18 20:24:37 -0700

    backup contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Schedule } from './schedule/schedule';
 
 export const routes: Routes = [
     {
@@ -15,6 +16,9 @@ export const routes: Routes = [
     {
         path: "contact",
         loadComponent: () => Contact
+    },
+    {
+        path: "schedule",
+        loadComponent: () => Schedule
     }
-
 ];
--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { APP_NAME, APP_TAGLINE, APP_PHONE, APP_HOURS, APP_EMAIL } from '../app.constants';
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
   imports: [],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-
+  appName = APP_NAME;
+  tagline = APP_TAGLINE;
+  phone = APP_PHONE;
+  hours = APP_HOURS;
+  email = APP_EMAIL;
 }
